9ba757ac1644df38192cc995217f85b833e3e3a5
ranger163 <ahmed.nassar163@gmail.com>
2021-02-08 16:21:19 +0300

    Adding NotificationUtils.kt

---
+++ app/src/main/java/me/inassar/androidnotifications/util/NotificationUtils.kt
@@ -0,0 +1,63 @@
+/*
+ * Copyright (C) 2019 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package me.inassar.androidnotifications.util
+
+import android.app.NotificationManager
+import android.content.Context
+
+// Notification ID.
+private val NOTIFICATION_ID = 0
+private val REQUEST_CODE = 0
+private val FLAGS = 0
+
+// TODO: Step 1.1 extension function to send messages (GIVEN)
+/**
+ * Builds and delivers the notification.
+ *
+ * @param context, activity context.
+ */
+fun NotificationManager.sendNotification(messageBody: String, applicationContext: Context) {
+    // Create the content intent for the notification, which launches
+    // this activity
+    // TODO: Step 1.11 create intent
+
+    // TODO: Step 1.12 create PendingIntent
+
+    // TODO: Step 2.0 add style
+
+    // TODO: Step 2.2 add snooze action
+
+    // TODO: Step 1.2 get an instance of NotificationCompat.Builder
+    // Build the notification
+
+    // TODO: Step 1.8 use the new 'breakfast' notification channel
+
+    // TODO: Step 1.3 set title, text and icon to builder
+
+    // TODO: Step 1.13 set content intent
+
+    // TODO: Step 2.1 add style to builder
+
+    // TODO: Step 2.3 add snooze action
+
+    // TODO: Step 2.5 set priority
+
+    // TODO: Step 1.4 call notify
+
+}
+
+// TODO: Step 1.14 Cancel all notifications
