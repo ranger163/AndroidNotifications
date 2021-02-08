3714ba5ab7727d45bd00fca2a6508f3beb87f414
ranger163 <ahmed.nassar163@gmail.com>
2021-02-08 16:23:54 +0300

    Displaying fragment into the activity

--- app/src/main/java/me/inassar/androidnotifications/MainActivity.kt
+++ app/src/main/java/me/inassar/androidnotifications/MainActivity.kt
@@ -2,10 +2,17 @@ package me.inassar.androidnotifications
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import me.inassar.androidnotifications.ui.EggTimerFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, EggTimerFragment.newInstance())
+                .commitNow()
+        }
     }
 }
