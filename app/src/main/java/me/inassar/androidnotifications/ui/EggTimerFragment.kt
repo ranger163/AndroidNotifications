208a502b9a7595ee9f2bf601296654ae97579575
ranger163 <ahmed.nassar163@gmail.com>
2021-02-08 16:19:53 +0300

    Adding EggTimerFragment.kt and it's fragment_egg_timer.xml

---
+++ app/src/main/java/me/inassar/androidnotifications/ui/EggTimerFragment.kt
@@ -0,0 +1,62 @@
+/*
+ * Copyright (C) 2019 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package me.inassar.androidnotifications.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
+import me.inassar.androidnotifications.R
+import me.inassar.androidnotifications.databinding.FragmentEggTimerBinding
+
+class EggTimerFragment : Fragment() {
+
+    private val TOPIC = "breakfast"
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        val binding: FragmentEggTimerBinding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_egg_timer, container, false
+        )
+
+        val viewModel = ViewModelProviders.of(this).get(EggTimerViewModel::class.java)
+
+        binding.eggTimerViewModel = viewModel
+        binding.lifecycleOwner = this.viewLifecycleOwner
+
+        // TODO: Step 1.7 call create channel
+
+        return binding.root
+    }
+
+    private fun createChannel(channelId: String, channelName: String) {
+        // TODO: Step 1.6 START create a channel
+
+        // TODO: Step 1.6 END create a channel
+
+    }
+
+    companion object {
+        fun newInstance() = EggTimerFragment()
+    }
+}
