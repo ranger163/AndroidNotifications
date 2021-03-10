4fd49cea9b9572f8368703e5a58cbe66458eacb4
ranger163 <ahmed.nassar163@gmail.com>
2021-03-10 15:51:27 +0300

    Adding new instance of notificationManager to cancel all notifications

--- app/src/main/java/me/inassar/androidnotifications/receiver/SnoozeReceiver.kt
+++ app/src/main/java/me/inassar/androidnotifications/receiver/SnoozeReceiver.kt
@@ -17,6 +17,7 @@
 package me.inassar.androidnotifications.receiver
 
 import android.app.AlarmManager
+import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
@@ -24,6 +25,7 @@ import android.content.Intent
 import android.os.SystemClock
 import android.text.format.DateUtils
 import androidx.core.app.AlarmManagerCompat
+import androidx.core.content.ContextCompat
 
 class SnoozeReceiver : BroadcastReceiver() {
     private val REQUEST_CODE = 0
@@ -45,6 +47,12 @@ class SnoozeReceiver : BroadcastReceiver() {
             triggerTime,
             notifyPendingIntent
         )
+
+        val notificationManager = ContextCompat.getSystemService(
+            context,
+            NotificationManager::class.java
+        ) as NotificationManager
+        notificationManager.cancelAll()
     }
 
 }
