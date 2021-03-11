6166af42faecac6b8c935f5af3ad81dfd8e7f7aa
ranger163 <ahmed.nassar163@gmail.com>
2021-03-11 11:02:46 +0300

    Adding MyFirebaseMessagingService.kt

---
+++ app/src/main/java/me/inassar/androidnotifications/MyFirebaseMessagingService.kt
@@ -0,0 +1,78 @@
+/*
+ * Copyright (C) 2019 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package me.inassar.androidnotifications
+
+import android.app.NotificationManager
+import android.util.Log
+import androidx.core.content.ContextCompat
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+import me.inassar.androidnotifications.util.sendNotification
+
+class MyFirebaseMessagingService : FirebaseMessagingService() {
+
+    /**
+     * Called when message is received.
+     *
+     * @param remoteMessage Object representing the message received from Firebase Cloud Messaging.
+     */
+    // [START receive_message]
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
+        Log.d(TAG, "From: ${remoteMessage?.from}")
+
+        // TODO Step 3.5 check messages for data
+        // Check if message contains a data payload.
+
+
+        // TODO Step 3.6 check messages for notification and call sendNotification
+        // Check if message contains a notification payload.
+    }
+
+    // [END receive_message]
+
+    //TODO Step 3.2 log registration token
+    // [START on_new_token]
+
+    // [END on_new_token]
+
+    /**
+     * Persist token to third-party (your app) servers.
+     *
+     * @param token The new token.
+     */
+    private fun sendRegistrationToServer(token: String?) {
+        // TODO: Implement this method to send token to your app server.
+    }
+
+    /**
+     * Create and show a simple notification containing the received FCM message.
+     *
+     * @param messageBody FCM message body received.
+     */
+    private fun sendNotification(messageBody: String) {
+        val notificationManager = ContextCompat.getSystemService(
+            applicationContext,
+            NotificationManager::class.java
+        ) as NotificationManager
+        notificationManager.sendNotification(messageBody, applicationContext)
+    }
+
+    companion object {
+        private const val TAG = "MyFirebaseMsgService"
+    }
+}
