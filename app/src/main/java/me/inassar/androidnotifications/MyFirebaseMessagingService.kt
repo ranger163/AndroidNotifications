388e7dba9416ef203ed87882f1f4433cc2aed308
ranger163 <ahmed.nassar163@gmail.com>
2021-03-11 11:05:34 +0300

    Implementing onNewToken function into  MyFirebaseMessagingService.kt

--- app/src/main/java/me/inassar/androidnotifications/MyFirebaseMessagingService.kt
+++ app/src/main/java/me/inassar/androidnotifications/MyFirebaseMessagingService.kt
@@ -47,11 +47,24 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
 
     //TODO Step 3.2 log registration token
     // [START on_new_token]
+    /**
+     * Called if InstanceID token is updated. This may occur if the security of
+     * the previous token had been compromised. Note that this is called when the InstanceID token
+     * is initially generated so this is where you would retrieve the token.
+     */
+    override fun onNewToken(token: String) {
+        Log.d(TAG, "Refreshed token: $token")
 
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+        sendRegistrationToServer(token)
+    }
     // [END on_new_token]
 
+
     /**
-     * Persist token to third-party (your app) servers.
+     * Persist token to third-party servers.
      *
      * @param token The new token.
      */
