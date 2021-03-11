6abe9d8e886a488aa9e3b8f8bd680543ae26e957
ranger163 <ahmed.nassar163@gmail.com>
2021-03-11 13:17:44 +0300

    Receiving notification data payload from FCM

--- app/src/main/java/me/inassar/androidnotifications/MyFirebaseMessagingService.kt
+++ app/src/main/java/me/inassar/androidnotifications/MyFirebaseMessagingService.kt
@@ -33,10 +33,13 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
     // [START receive_message]
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
         // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
-        Log.d(TAG, "From: ${remoteMessage?.from}")
+        Log.d(TAG, "From: ${remoteMessage.from}")
 
         // TODO Step 3.5 check messages for data
         // Check if message contains a data payload.
+        remoteMessage.data.let {
+            Log.d(TAG, "Message data payload: ${remoteMessage.data}")
+        }
 
 
         // TODO Step 3.6 check messages for notification and call sendNotification
