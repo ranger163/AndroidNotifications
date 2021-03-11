f4e48f2c724fa453df671a2fcac14460f8057ee7
ranger163 <ahmed.nassar163@gmail.com>
2021-03-11 14:46:42 +0300

    check messages for notification and call sendNotification. Check if message contains a notification payload.

--- app/src/main/java/me/inassar/androidnotifications/MyFirebaseMessagingService.kt
+++ app/src/main/java/me/inassar/androidnotifications/MyFirebaseMessagingService.kt
@@ -35,15 +35,19 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
         // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
         Log.d(TAG, "From: ${remoteMessage.from}")
 
-        // TODO Step 3.5 check messages for data
+        // Step 3.5 check messages for data
         // Check if message contains a data payload.
         remoteMessage.data.let {
             Log.d(TAG, "Message data payload: ${remoteMessage.data}")
         }
 
 
-        // TODO Step 3.6 check messages for notification and call sendNotification
+        // Step 3.6 check messages for notification and call sendNotification
         // Check if message contains a notification payload.
+        remoteMessage.notification.let {
+            Log.d(TAG, "Message Notification Body: ${it?.body}")
+            sendNotification(it?.body as String)
+        }
     }
 
     // [END receive_message]
